05f48951da2dfead188d69738482e0fc6dcbe9b9
Jonathan Morris <jonathan_wellingoton@icloud.com>
2024-09-08 21:14:52 +0000

    cooked

--- backend/src/control/rag_functions.py
+++ backend/src/control/rag_functions.py
@@ -12,7 +12,7 @@ from llama_index.llms.openai import OpenAI
 from dotenv import load_dotenv
 
 
-CHROMA_PATH = "src/data/chroma"
+CHROMA_PATH = "src/data/"
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -22,7 +22,6 @@ api_key = os.getenv("OPENAI_API_KEY")
 # IMAGE_RUN = ???
 
 def get_retriever(k = 3):
-    
     db = chromadb.PersistentClient(path=CHROMA_PATH)
     chroma_collection = db.get_or_create_collection("chroma_db")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
@@ -49,15 +48,16 @@ def query(retriever, query_text: str):
     llm = OpenAI(model = "gpt-3.5-turbo", api_key=api_key)
 
     prompt = PromptTemplate(
-    "Context information is below.\n"
-    "---------------------\n"
-    "{context_str}\n"
-    "---------------------\n"
-    "Given the context information and not prior knowledge, "
-    "answer the query.\n"
-    "Query: {query_str}\n"
-    "Answer: "
+        "Context information is below.\n"
+        "---------------------\n"
+        "{context_str}\n"
+        "---------------------\n"
+        "Given the context information and not prior knowledge, "
+        "answer the query.\n"
+        "Query: {query_str}\n"
+        "Answer: "
     )
+
     formatted_prompt = prompt.format(context_str=context, query_str=query_text)
     
     response = llm.complete(formatted_prompt)
